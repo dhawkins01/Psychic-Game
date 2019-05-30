be61a6976bf7cdcd1b691ffb6bd5ff73ff71a5fd
Dave Hawkins <dhawkins01@gmail.com>
2019-05-29 20:25:35 -0400

    declaring variable is the js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var wins = 0;
+var loses = 0;
+var gueses = 9;
+var guessSoFar = [];
+var psychicLetter;
