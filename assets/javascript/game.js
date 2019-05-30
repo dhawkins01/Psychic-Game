ab2cb66fe4c9bcc2001df17fa787d452697ffc33
Dave Hawkins <dhawkins01@gmail.com>
2019-05-29 21:19:53 -0400

    finished the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+// declare all the global variables
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
@@ -26,6 +28,7 @@ var newGame = function() {
     newLetter();
     guessesLeft();
     soFar();
+    guessesSoFar = [];
     
 }
 
